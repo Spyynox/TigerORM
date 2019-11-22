a9a38d975114da380f2f6aa68c4a3ae440b26cd1
Anthony <anthonyd-m@live.com>
2019-11-22 15:17:02 +0100

    created index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$conn = [
+    "driver" => "pdo_mysql",
+    "host" => "localhost",
+    "dbname" => "phpcinema",
+    "user" => "root",
+    "password" => "root",
+    "port" => "8889",
+];
