5aa3c468a9eacc280f2128304d461e629302dbf5
Anthony <anthonyd-m@live.com>
2019-12-26 11:14:33 +0100

    added update, delete, count and exists

--- src/TigerORM/TigerORM.php
+++ src/TigerORM/TigerORM.php
@@ -43,7 +43,7 @@ class TigerORM {
     }
 
     public function findBy($class, $fields, $values) {
-        $where = array_map($this->addQuestionMarks, $fields);
+        $where = array_map($this->addQuestionMarks, $fields).\join(" AND ");
         $req = $pdo->prepare('SELECT * FROM '.$class.' WHERE '.$where);
         $req->execute($id);
         $objs = $this->fetchAllObjects($req, $class);
@@ -60,12 +60,46 @@ class TigerORM {
     }
 
     public function findByOrdered($class, $fields, $values, $orderBy) {
-        $where = array_map($this->addQuestionMarks, $fields);
+        $where = array_map($this->addQuestionMarks, $fields).\join(" AND ");
         $req = $pdo->prepare('SELECT * FROM '.$class.' WHERE '.$where.' ORDER BY '.$orderBy);
         $req->execute($id);
         $objs = $this->fetchAllObjects($req, $class);
 
         return $objs;
+    }
+
+    public function update($class, $fieldsToUpdate, $newValues, $conditionField, $conditionValue) {
+        $updates = \array_map($this->addQuestionMarks, $fieldsToUpdate).\join(", ");
+        $where = \array_map($this->addQuestionMarks, $conditionField).\join(" AND ");
+        $req = $pdo->prepare('UPDATE '.$class.' SET '.$updates.' WHERE '.$where);
+        $req->execute(array_push($newValues, $conditionValue));
+        $objs = $this->fetchAllObjects($req, $class);
+
+        return $objs;
+    }
+
+    public function delete($class, $fields, $values) {
+        $where = \array_map($this->addQuestionMarks, $fields).\join(" AND ");
+        $req = $pdo->prepare("DELETE FROM ".$class." WHERE ".$where);
+        $req->execute($values);
+        $objs = $this->fetchAllObjects($req, $class);
+
+        return $objs;
+    }
+
+    public function count($class, $fieldToCount) {
+        $req = $pdo->prepare("SELECT COUNT(".$fieldToCount.") FROM ".$class);
+        $req->execute();
+
+        return $req->fetchColumn();
+    }
+
+    public function exists($class, $fields, $values) {
+        $where = \array_map($this->addQuestionMarks, $fields).\join(" AND ");
+        $req = $pdo->prepare("SELECT * FROM ".$class." WHERE EXISTS ( SELECT * FROM ".$class." WHERE ".$where);
+        $req->execute($values);
+        
+        return $req->fetchColumn();
     }
 
     private function fetchAllObjects($req, $class) {
@@ -92,5 +126,8 @@ class TigerORM {
     }
 
     // see fetchObject PDO
+    // see https://stackoverflow.com/questions/19898688/how-to-create-a-logfile-in-php for the logs
+    // see https://stackoverflow.com/questions/15275689/error-checking-for-pdo-prepared-statements for the errors
+    // https://www.php.net/manual/fr/pdo.errorinfo.php
 
 }
